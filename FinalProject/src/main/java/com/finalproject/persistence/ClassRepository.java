6502608c657e5b21245d93c2ec8a435e8b3eb462
girin17 <02snake@naver.com>
2023-12-07 13:44:57 +0900

    불필요한 java 파일 삭제

--- FinalProject/src/main/java/com/finalproject/persistence/ClassRepository.java
+++
@@ -1,8 +0,0 @@
-package com.finalproject.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.finalproject.domain.ClassEntity;
-
-public interface ClassRepository extends JpaRepository<ClassEntity, Long> {
-}
